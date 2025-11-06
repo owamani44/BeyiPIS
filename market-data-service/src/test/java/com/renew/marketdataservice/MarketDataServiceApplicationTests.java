da9d58fc90905025fb5b17e4b1c0cf66d109dd4e
Solomon Owamani <owamani44@gmail.com>
2025-11-06 22:18:53 +0300

    Initial Commit

---
+++ market-data-service/src/test/java/com/renew/marketdataservice/MarketDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renew.marketdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-service/src/test/java/com/renew/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renew.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
