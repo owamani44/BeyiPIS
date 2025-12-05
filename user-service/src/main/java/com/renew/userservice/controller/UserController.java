6fa065b81223326b5c0d2527fce26b90a0a48d27
Solomon Owamani <owamani44@gmail.com>
2025-12-05 13:16:23 +0300

    5 DEC

--- user-service/src/main/java/com/renew/userservice/controller/UserController.java
+++ user-service/src/main/java/com/renew/userservice/controller/UserController.java
@@ -4,6 +4,8 @@ package com.renew.userservice.controller;
 import com.renew.userservice.dto.userDTOs.UserRequestDTO;
 import com.renew.userservice.dto.userDTOs.UserResponseDTO;
 import com.renew.userservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Tag(name = "Users",description ="API for managing users")
 public class UserController {
 
     private final UserService userService;
@@ -25,12 +28,14 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Get users")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
         List<UserResponseDTO> users = userService.getUsers();
         return ResponseEntity.ok().body(users);
     }
 
     @PostMapping
+    @Operation(summary = "Create users")
     public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO){
         UserResponseDTO userResponseDTO = userService.createUser(userRequestDTO);
         return ResponseEntity.ok().body(userResponseDTO);
@@ -43,6 +48,7 @@ public class UserController {
 //    }
 
     @PutMapping("/{userid}")
+    @Operation(summary = "update users")
     public ResponseEntity<UserResponseDTO> updateUser(@PathVariable UUID userid,
                                                       @Validated({Default.class})
                                                           @RequestBody UserRequestDTO userRequestDTO){
@@ -51,6 +57,7 @@ public class UserController {
     }
 
     @DeleteMapping("/{userid}")
+    @Operation(summary = "Delete users")
     public ResponseEntity<UserResponseDTO> deleteUser(@PathVariable UUID userid){
         userService.deleteUser(userid);
         return ResponseEntity.noContent().build();
